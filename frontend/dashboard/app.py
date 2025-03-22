db6d8ee6d843b6c7cbfd0da2608688d514ea0afb
Jack Carroll <carroll.jac@northeastern.edu>
2025-03-22 16:57:25 -0400

    basics of eda and data communication on frontend

--- frontend/dashboard/app.py
+++ frontend/dashboard/app.py
@@ -7,8 +7,8 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../'
 import pandas as pd
 import numpy as np
 import streamlit as st
-
-from backend.data_collection import fetch_stock_data
+from backend.data_cleaning import clean_and_prepare_data
+from backend.data_collection import fetch_stock_data, save_stock_data_toCSV
 
 # st.title('Monte Carlo Asset Pricing Simulation')
 st.title('📈 Stock Data Viewer')
@@ -21,6 +21,15 @@ ticker = st.text_input("Enter Stock Ticker (e.g., MSFT, AAPL):", value="MSFT")
 start_date = st.date_input("Select Start Date")
 end_date = st.date_input("Select End Date")
 
+if ticker:
+    st.session_state.ticker = ticker
+
+if start_date:
+    st.session_state.start_date = start_date
+
+if end_date:
+    st.session_state.end_date = end_date
+
 intervals = ["1m", "2m", "5m", "15m", "30m", "60m", "90m", "1h", "1d", "5d", "1wk", "1mo", "3mo"]
 interval = st.selectbox("Select Interval", intervals, index=intervals.index("1d"))
 
@@ -30,6 +39,12 @@ if st.button("Fetch Data"):
         st.error("End date must be after start date.")
     else:
         df = fetch_stock_data(ticker, str(start_date), str(end_date), interval)
+        current_file = os.path.abspath(__file__)
+        project_root = os.path.abspath(os.path.join(current_file, "../../../"))
+        data_folder = os.path.join(project_root, "data", "processed")
+        filename = f"{ticker}_{start_date}_to_{end_date}_processed.csv"
+        file_path = os.path.join(data_folder, filename)
+        clean = clean_and_prepare_data(df, save_path = file_path)
         if df is not None:
             st.subheader(f"Stock Data for {ticker}")
             st.write(df)
--- frontend/dashboard/pages/data_viz.py
+++ frontend/dashboard/pages/data_viz.py
@@ -1,6 +1,35 @@
 import streamlit as st
 import altair as alt
 import pandas as pd
+import os
 
 st.title("Exploratory Data Analysis")
 
+current_file = os.path.abspath(__file__)
+project_root = os.path.abspath(os.path.join(current_file, "../../../../"))
+data_folder = os.path.join(project_root, "data", "processed")
+
+ticker = st.session_state.get('ticker')
+start_date = st.session_state.get('start_date')
+end_date = st.session_state.get('end_date')
+
+
+filename = f"{ticker}_{start_date}_to_{end_date}_processed.csv"
+data_path = os.path.join(data_folder, filename)
+data = pd.read_csv(data_path)
+
+close_line = alt.Chart(data).mark_line(color = "red").encode(
+    x = "Date",
+    y = "Close"
+).interactive()
+
+return_line = alt.Chart(data).mark_line(color = "blue").encode(
+    x = "Date",
+    y = "Daily_Return"
+).interactive()
+
+st.subheader(f"Closing Price From {start_date} to {end_date}")
+st.altair_chart(close_line)
+
+st.subheader(f"Daily Returns From {start_date} to {end_date}")
+st.altair_chart(return_line)
